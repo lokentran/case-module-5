da4b2c9da5d9f5a5d8eadbe30102f3f1fbfa781b
lokentran <thanhtungmarine@gmail.com>
2020-10-08 22:24:31 +0700

    Refactor Route

--- routes/web.php
+++ routes/web.php
@@ -25,25 +25,36 @@ Route::get('/logout', '\App\Http\Controllers\UserController@logout')->name('logi
 Route::get('/register', '\App\Http\Controllers\UserController@showFormRegister');
 Route::post('/register', '\App\Http\Controllers\UserController@register')->name('register');
 
-Route::get('/user/{id}/profile', '\App\Http\Controllers\UserController@showProfile')->name('profile.show')->middleware('auth');
-Route::post('/user/{id}/profile', '\App\Http\Controllers\UserController@updateProfile')->name('profile.update')->middleware('auth');
-Route::get('/user/{id}/change-pass', '\App\Http\Controllers\UserController@showFormChangePass')->name('user.showPass')->middleware('auth');
-Route::post('/user/{id}/change-pass', '\App\Http\Controllers\UserController@updatePass')->name('user.updatePass')->middleware('auth');
-Route::get('/user/{id}/list-house', '\App\Http\Controllers\HouseController@showCustomerHouse')->name('user.showCustomerHouse')->middleware('auth');
-Route::get('/user/{id}/order-house', '\App\Http\Controllers\HouseController@showListHouse')->name('user.showListHouse')->middleware('auth');
-
-
+Route::get('/', '\App\Http\Controllers\FrontendController@showIndex')->name('index');
 Route::post('/search', '\App\Http\Controllers\HouseController@search')->name('house.search');
 
-Route::get('/', '\App\Http\Controllers\FrontendController@showIndex')->name('index');
+Route::group(['middleware' => ['auth']], function () {
+    Route::group(['prefix' => 'user'], function () {
+        Route::get('/{id}/profile', '\App\Http\Controllers\UserController@showProfile')->name('profile.show');
+        Route::post('/{id}/profile', '\App\Http\Controllers\UserController@updateProfile')->name('profile.update');
+        Route::get('/{id}/change-pass', '\App\Http\Controllers\UserController@showFormChangePass')->name('user.showPass');
+        Route::post('/{id}/change-pass', '\App\Http\Controllers\UserController@updatePass')->name('user.updatePass');
+        Route::get('/{id}/list-house', '\App\Http\Controllers\HouseController@showCustomerHouse')->name('user.showCustomerHouse');
+        Route::get('/{id}/order-house', '\App\Http\Controllers\HouseController@showListHouse')->name('user.showListHouse');
+        Route::get('/add-house', '\App\Http\Controllers\HouseController@showFormAdd')->name('house.add')->middleware('auth');
+        Route::post('/add-house', '\App\Http\Controllers\HouseController@postHouse')->name('house.addForm')->middleware('auth');
+    });
 
-Route::get('/user/add-house', '\App\Http\Controllers\HouseController@showFormAdd')->name('house.add')->middleware('auth');
-Route::post('/user/add-house', '\App\Http\Controllers\HouseController@postHouse')->name('house.addForm')->middleware('auth');
+    Route::group(['prefix' => 'house'], function () {
+        Route::get('/{id}/detail', '\App\Http\Controllers\HouseController@detail')->name('house.detail');
+        Route::post('/{id}/detail/', '\App\Http\Controllers\HouseController@rentHome')->name('house.rent');
+        Route::get('/{id}/detail/confirm', '\App\Http\Controllers\BillController@confirmIndex')->name('house.confirm');
+        Route::post('/{id}/detail/confirm', '\App\Http\Controllers\BillController@confirmPost')->name('house.confirmPost');
+        Route::get('/confirm/success', '\App\Http\Controllers\BillController@success')->name('bill.success');
+    });
+});
 
-Route::get('/house/{id}/detail', '\App\Http\Controllers\HouseController@detail')->name('house.detail');
-Route::post('/house/{id}/detail/', '\App\Http\Controllers\HouseController@rentHome')->name('house.rent')->middleware('auth');
 
-Route::get('/house/{id}/detail/confirm', '\App\Http\Controllers\BillController@confirmIndex')->name('house.confirm')->middleware('auth');
-Route::post('/house/{id}/detail/confirm', '\App\Http\Controllers\BillController@confirmPost')->name('house.confirmPost')->middleware('auth');
 
-Route::get('/confirm/success', '\App\Http\Controllers\BillController@success')->name('bill.success')->middleware('auth');
+
+
+
+
+
+
+
